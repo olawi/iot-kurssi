9679d1dd6bff3c6caa14e6979a8b106e4b869bb5
Paavo Pietarila <paavo.pietarila@gmail.com>
2019-11-13 11:20:38 +0200

    first hello in child proc

---
+++ helloexpress/app.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const process = require("child_process");
+
+let app = express();
+
+app.get("/", function(req, res) {
+    //    return res.status(200).json({message:"Hello world!"});
+
+    let proc = process.exec("./hello.out", function(error, stdout, stderr) {
+	if(error) {
+	    console.log("Failed to spawn child process: ", error);
+	    return res.status(404).json({message:"Not found"});
+	}
+	if(stdout) {
+	    return res.status(200).json({message:stdout});
+	}
+	return res.status(404).json({message:"Not found"});
+    })
+    console.log("Started process with PID:",proc.pid);
+    
+});
+
+app.listen(3000);
+console.log("Running in port 3000");
