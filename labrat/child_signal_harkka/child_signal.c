6bd78e62bd2e5dcd7b3028a102f9ac707b00fa3d
Paavo Pietarila <paavo.pietarila@gmail.com>
2019-11-18 22:22:42 +0200

    added child create and cill functions and signal handler stub.

--- labrat/child_signal_harkka/child_signal.c
+++ labrat/child_signal_harkka/child_signal.c
@@ -65,37 +65,66 @@ void child_process_loop()
 	}
 }
 
-int create_child(int pid) {
+int _create_child(int pid) {
 /**
  * Creates a child and puts it to work when pid is zero,
  * does nothing otherwise
+ * @return pid of the child process
  */
-	debug_out("ENTER create_child\n");
+	debug_out("ENTER create_child \n");
 	if (pid == 0)
 	{
 		pid = fork();
 		if (pid > 0)
 		{
-			debug_out("PARENT in create_child\n");
+			debug_out("PARENT in create_child \n");
 			// this is the parent code, return pid
 			return pid;
 		}
 		else
 		{
-			debug_out("CHILD in create_child\n");
+			debug_out("CHILD in create_child \n");
 			// this is the child code, enter into eternal labour
 			child_process_loop();
 			// we never should get here
-			debug_out("ERROR in create child code\n");
+			debug_out("ERROR in create child code \n");
 			exit(1);
 		}
 	}
 	else
 	{
-		debug_out("CHILD EXISTS in create_child\n");
+		debug_out("CALLED WITH EXISTING PID in create_child \n");
 		// leave things as they are
 		return pid;
 	}
+}
+
+int create_child() {
+	/**
+	 * Wrapper to create a new child if there are not too many already
+	 * @return child PID, or 0 for error
+	*/
+	int i, pid = 0;
+	int c_idx = -1;
+
+	// first PID from the table that is zero
+	i = 0;
+	while((i < MAX_CHILD_COUNT) && (g_cpid_table[i] != 0)) {
+		i++;
+	}
+
+	if(i < MAX_CHILD_COUNT) {
+		c_idx = i;
+	} else {
+		debug_out("MAX NUMBER OF CHILDRED REACHED \n");
+		return 0;
+	}
+
+	pid = _create_child(0);
+	g_cpid_table[c_idx] = pid;
+	g_children++;
+
+	return pid;
 }
 
 int kill_child(int pid) {
@@ -106,50 +135,82 @@ int kill_child(int pid) {
 	int c_in_table = 0;
 	int c_idx = -1;
 
-	/** Check the pid against the global child table */
-	for(i=0;i>MAX_CHILD_COUNT;i++) {
-		if(g_cpid_table[i] == pid) {
+	if(pid == 0) {
+		// Can be called with 0 to do nothing 
+		debug_out("EMPTY PID in kill_child \n");
+		return 0;
+	}
+
+	// Check the pid against the global child table
+	for(i=0; i < MAX_CHILD_COUNT; i++) {
+		if(pid == g_cpid_table[i]) {
 			c_in_table = 1;
 			c_idx = i;
 		}
 	}
 
-	if(!c_in_table || (pid == 0)) {
-		debug_out("CHILD PID NOT EXIST in kill_child");
+	if(!c_in_table) {
+		debug_out("CHILD PID NOT EXIST in kill_child \n");
 		return 0;
 	}
 
-	if (pid != 0)
+	if (pid && c_in_table)
+	// pid is non-zero and valid
 	{
-		debug_out("KILL in kill_child\n");
+		debug_out("KILL in kill_child \n");
 		// honey, we have a child
 		kill(pid, SIGKILL);
 		// let's wait for it to die
 		wait(&wstatus);
 
-		/* WRITE TO THE GLOBAL PID BUFFER */
+		// clear the pid table entry
 		g_cpid_table[c_idx] = 0;
-
+		g_children--;
 		return wstatus;
 	}
-	else
-	{
-		debug_out("NO CHILD TO KILL in kill_child\n");
-		// do nothing
-		return 0;
-	}
+	// We should never get here
+	debug_out("ERROR in control \n");
+	return 0;
 }
 
-    /* tehtävänantoa : */
-    // TODO: arvo luku 11-20 välillä. Printtaa ulos luku aikana, jonka lapsi nukkuu.
-    // Nuku arvottu aika. Printtaa ulos että lapsi kuolee ja sen jälkeen exit.
-    // Googleta miten C:n random toimii ellei ole tuttu
+int kill_all_children() {
+	/** what it says */
+	int i, pid, count=0;
+	for(i=0; i < MAX_CHILD_COUNT; i++) {
+		pid = g_cpid_table[i];
+		// printf("Killing process %d \n", pid);
+		if(kill_child(pid)) {
+			count++;
+		}
+	}
+	return count;
+}
+
+/* tehtävänantoa : */
+// TODO: arvo luku 11-20 välillä. Printtaa ulos luku aikana, jonka lapsi nukkuu.
+// Nuku arvottu aika. Printtaa ulos että lapsi kuolee ja sen jälkeen exit.
+// Googleta miten C:n random toimii ellei ole tuttu
 
 
-void myHandler(int signum)
+void my_sig_handler(int signum)
 {
-    // TODO: vähennä globaalia children arvoa yhdellä.
+    // catch the sigchild signal and update child table and count
     g_children--;
+	debug_out("* caught SIGCHLD ");
+}
+
+void init_signals()
+{
+	struct sigaction sa;
+	sa.sa_handler = &my_sig_handler;
+	sa.sa_flags = 0;
+	sigfillset(&sa.sa_mask);
+
+	if (sigaction(SIGCHLD, &sa, NULL) == -1)
+	{
+		printf("Error installing sighandler, exiting \n");
+		exit(1);
+	}
 }
 
 void init_() {
@@ -158,14 +219,16 @@ void init_() {
  */
     int i;
     
-    for(i=0;i<MAX_CHILD_COUNT;i++) {
+    for(i=0; i < MAX_CHILD_COUNT; i++) {
 		g_cpid_table[i] = 0;
     }
+
+	init_signals();
+
 }
 
 int main(int argc, char **argv)
 {
-    //struct sigaction sa;
 
     /* tehtävänantoa : */
     // TODO: Luo sigaction struct ,  alusta mask ja flags kaikki // signaalit ja asenna signal handler SIGCHLD signaalille.
@@ -179,7 +242,7 @@ int main(int argc, char **argv)
     /* testing testing */
 
     static CHILD_DATA_TYPE tmp_buffer[CHILD_BUFFER_SIZE];
-    int i;
+    int pid, i;
 
     /* test that the buffer workx */
     int n = 0;
@@ -192,28 +255,22 @@ int main(int argc, char **argv)
     init_();
     
     /* First create children */
-    for (i=0;i<MAX_CHILD_COUNT;i++) {
-		g_cpid_table[i] = create_child(g_cpid_table[i]);
+ /*
+    for (i=0; i <MAX_CHILD_COUNT; i++) {
+		pid = _create_child(g_cpid_table[i]);
+		g_cpid_table[i] = pid;
+		printf("Created child process with PID %d \n", g_cpid_table[i]);
     }
-    
-	/* This should not work, or report that the child exists */
-    for (i=0;i<MAX_CHILD_COUNT;i++) {
-		g_cpid_table[i] = create_child(g_cpid_table[i]);
+*/ 
+
+	// more testing testing
+    for (i=0; i < (MAX_CHILD_COUNT + 2); i++) {
+		create_child();
     }
     
 	sleep(2);
 
-	/* Kill children one by one */
-    for (i=0;i<MAX_CHILD_COUNT;i++) {
-		kill_child(g_cpid_table[i]);
-    }
-    
-	/* Kill them dead (actually report that nothing to be killed) */
-    for (i=0;i<MAX_CHILD_COUNT;i++) {
-		kill_child(g_cpid_table[i]);
-    }
-    
-
-
+	kill_all_children();
+	kill_all_children();
     
 }
