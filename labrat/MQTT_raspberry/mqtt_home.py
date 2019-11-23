8a8790d2ee8c36829028e82094205543cfa6af2b
Paavo Pietarila <paavo.pietarila@gmail.com>
2019-11-23 13:06:34 +0200

    test code. will rewrite all to a new dir

--- labrat/MQTT_raspberry/mqtt_home.py
+++ labrat/MQTT_raspberry/mqtt_home.py
@@ -1,8 +1,9 @@
 import sys
 import time
 import pymongo
-import Adafruit_DHT #uncomment this line if you have DHT sensor connected to RPi
+#import Adafruit_DHT #uncomment this line if you have DHT sensor connected to RPi
 import paho.mqtt.client as mqtt
+import json
 import datetime
 import re
 
@@ -12,6 +13,8 @@ def debug(text):
         print(text)
 
 USE_MONGO = 1
+
+db_client = pymongo.MongoClient()
 
 def connect_mongo():
     """ Read credentials from local file .mongopass and connect to database and collection """
@@ -23,14 +26,17 @@ def connect_mongo():
         fd.close()
 
     #Establish connection to MongoDB
-    client = pymongo.MongoClient(mongoargs[0])
+    db_client = pymongo.MongoClient(mongoargs[0])
 
     #Db and Collection
-    mydb = client[mongoargs[1]]
+    mydb = db_client[mongoargs[1]]
     mycol = mydb[mongoargs[2]]
 
     #Returns handle to the collection
     return mycol
+
+def close_mongo():
+    db_client.close() 
 
 def write_to_db(data):
     """ write json to DB """
@@ -38,7 +44,7 @@ def write_to_db(data):
     debug('Writing '+data+' to DB...')
     db.insert_one(json.loads(data))
     debug('success!')
-    db.close()
+    close_mongo()
 
 """ Global variables for MQTT """
 broker_address = "broker.hivemq.com"
@@ -46,21 +52,23 @@ mqtt_request_str = "oksanen/willab"
 mqtt_publish_str = "oksanen/willab/ID1"
 
 
-client = mqtt.Client()
+mqtt_client = mqtt.Client()
 id = "ID1"
 
 def on_connect(client, userdata, flags, rc):
-    client.subscribe(mqtt_request_str)
+    mqtt_client.subscribe(mqtt_request_str)
     print("Connected to " + broker_address)
 
 def on_message(client, userdata, msg):
     print("Received MQTT request")
     print(msg.payload)
 
-    humidity, temperature = Adafruit_DHT.read_retry(11, 4)
+    temperature = 0.666
+    humidity = 0.777
+ #    humidity, temperature = Adafruit_DHT.read_retry(11, 4)
 
     # HOX: this fails if there is no \d+ in msp.payload
-    req = re.findall('\d+', str(msg.payload))[0]
+    req = re.findall(r'\d+', str(msg.payload))[0]
     timestamp = datetime.datetime.now().strftime("%Y-%m-%dT%H:%M:%S")
 
     reply = '{{"Req":{},"Time":"{}","ID":"{}","T":{:0.1f},"H":{:0.1f}}}'.format(req, timestamp, id, temperature, humidity)
@@ -68,19 +76,25 @@ def on_message(client, userdata, msg):
     reply = '{"tag":"test","number":2.0}'
 
     print(reply)
-    client.publish(mqtt_publish_str, reply)
+    mqtt_client.publish(mqtt_publish_str, reply)
 
     if USE_MONGO:
         write_to_db(reply)
 
     print('end of on_message')
 
+while(1):
+    dummy_data = '{"tag":"test","data":3.11}'
+    print("Write")
+    write_to_db(dummy_data)
+    print("OK, waiting...")
+    time.sleep(5)
 
-client.on_connect = on_connect
-client.on_message = on_message
-client.connect(broker_address)
+mqtt_client.on_connect = on_connect
+mqtt_client.on_message = on_message
+mqtt_client.connect(broker_address)
 
 try:
-    client.loop_forever()
+    mqtt_client.loop_forever()
 except:
     print("error")
